1b730056aea65facbf5662d4151da68cf7c9a767
UIT-20521205 <dinhiendung1026@gmail.com>
2023-04-24 00:42:38 +0700

    screenlook

--- Assets/Scripts/Bullet/BulletFly.cs
+++ Assets/Scripts/Bullet/BulletFly.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BulletFly : Flyable
 {
+    private float rotSpeed = 1.0f;
     protected override void ResetValue()
     {
         base.ResetValue();
   
+    }
+
+    protected override void Fly()
+    {
+        base.Fly();
     }
 }
